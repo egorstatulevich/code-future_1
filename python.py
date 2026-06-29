cc3e1e2201ef2925891e806b8534bfc0483eed9b
a-nasibjan <nasibyan.albert@gmail.com>
2026-06-29 23:48:00 +0300

    inintal commit

---
+++ python.py
@@ -0,0 +1,11 @@
+def load_books():
+    pass
+def save_books(books):
+    pass
+def main():
+    while True:
+        # Показать меню
+        # Вызвать нужную функцию
+        break
+if __name__ == "__main__":
+    main()
